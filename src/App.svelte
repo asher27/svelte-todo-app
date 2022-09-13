1bccbf023248528db69a5efc747ea1dced0a3ec5
asher27 <asher1225@gmail.com>
2022-09-13 11:34:55 +0900

    Create Project

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import CreateTodo from '~/components/CreateTodo'
+  import Todo from '~/components/Todo'
+  import {todos} from '~/store'
+
+  const storageTodos = localStorage.getItem('todos')
+  if (storageTodos) {
+    $todos = JSON.parse(storageTodos)
+  }
+</script>
+
+<div class="container">
+  <CreateTodo />
+  <div class="todos">
+    {#each $todos as todo (todo.id) }
+    <Todo todo={todo} />
+    {/each}
+  </div>
+</div>
+
+<style lang="scss">
+  .todos {
+    padding: 20px 0;
+  }
+</style>
---
+++ src/components/CreateTodo.svelte
@@ -0,0 +1,47 @@
+<script>
+  import {todos, saveStorage} from '~/store'
+  import shortid from 'shortid'
+  let title = ''
+
+  function createTodo() {
+
+    if (!title || !title.trim()) return
+
+    $todos.unshift({
+      id: shortid.generate(),
+      title: title 
+    })
+    $todos = $todos
+    saveStorage()
+
+    title = ''
+    console.log($todos)
+  }
+</script>
+
+<div class="create-todo">
+  <input type="text"
+  bind:value={title}
+  class="form-control"
+  on:keyup={e => {
+  if (e.key === 'Enter') createTodo()  
+  }} />
+  <button class="btn btn-primary"
+  on:click={createTodo}
+  >CreateTodo</button>
+</div>
+
+<style lang="scss">
+  .create-todo {
+
+    display:flex;
+    margin-top: 50px;
+    .btn {
+      width: 130px;
+      height: 50px;
+      font-weight: 700;
+      margin-left: 10px;
+      flex-shrink: 0;
+    }
+  }
+</style>
